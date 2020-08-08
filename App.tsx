846b342655aaaec040ae8e7cb734a6b2793397bf
KievL <kiev.sider@hotmail.com>
2020-08-07 21:27:39 -0300

    hi

--- App.tsx
+++ App.tsx
@@ -1,23 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Landing from './src/pages/Landing';
+import { View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-
-      <Text>Okay</Text>
-
+    <>
+      <Landing />
       <StatusBar style="auto" />
-    </View>
+
+    </>        
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
